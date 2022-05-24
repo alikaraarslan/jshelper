f552dea63c89521d2af5600acb5a34079c53fe70
Ali Karaarslan <ali.karaarslan@bilgeadam.com>
2022-05-25 01:36:03 +0300

    update sidebarMenuItems without empty component

--- assets/db/sidebarMenuItems.js
+++ assets/db/sidebarMenuItems.js
@@ -76,239 +76,239 @@ export const sidebarMenuItems = [
         title: 'Spread operator',
         path: '/helpers/es6/spread-operator',
       },
-      {
-        id: '2.9',
-        title: 'String includes()',
-        path: '/helpers/es6/string-includes',
-      },
-      {
-        id: '3',
-        title: 'String startsWith()',
-        path: '/helpers/es6/string-starts-with',
-      },
-      {
-        id: '3.1',
-        title: 'String repeat()',
-        path: '/helpers/es6/string-repeat',
-      },
-      {
-        id: '3.2',
-        title: 'Destructuring array',
-        path: '/helpers/es6/destructuring-array',
-      },
-      {
-        id: '3.3',
-        title: 'Destructuring object',
-        path: '/helpers/es6/destructuring-object',
-      },
-      {
-        id: '3.4',
-        title: 'Object property assignment',
-        path: '/helpers/es6/object-property-assignment',
-      },
-      {
-        id: '3.5',
-        title: 'Object.assign()',
-        path: '/helpers/es6/object-assign',
-      },
-      {
-        id: '3.6',
-        title: 'Promises with finally',
-        path: '/helpers/es6/promises-with-finally',
-      },
-      {
-        id: '3.7',
-        title: 'Object function assignment',
-        path: '/helpers/es6/object-function-assignment',
-      },
-      {
-        id: '3.8',
-        title: 'Object.entries()',
-        path: '/helpers/es6/object-entries',
-      },
-      {
-        id: '3.9',
-        title: 'Object Spread operator',
-        path: '/helpers/es6/object-spread-operator',
-      },
-      {
-        id: '4.0',
-        title: 'Destructuring Nested Objects',
-        path: '/helpers/es6/destructuring-nested-objects',
-      },
+      // {
+      //   id: '2.9',
+      //   title: 'String includes()',
+      //   path: '/helpers/es6/string-includes',
+      // },
+      // {
+      //   id: '3',
+      //   title: 'String startsWith()',
+      //   path: '/helpers/es6/string-starts-with',
+      // },
+      // {
+      //   id: '3.1',
+      //   title: 'String repeat()',
+      //   path: '/helpers/es6/string-repeat',
+      // },
+      // {
+      //   id: '3.2',
+      //   title: 'Destructuring array',
+      //   path: '/helpers/es6/destructuring-array',
+      // },
+      // {
+      //   id: '3.3',
+      //   title: 'Destructuring object',
+      //   path: '/helpers/es6/destructuring-object',
+      // },
+      // {
+      //   id: '3.4',
+      //   title: 'Object property assignment',
+      //   path: '/helpers/es6/object-property-assignment',
+      // },
+      // {
+      //   id: '3.5',
+      //   title: 'Object.assign()',
+      //   path: '/helpers/es6/object-assign',
+      // },
+      // {
+      //   id: '3.6',
+      //   title: 'Promises with finally',
+      //   path: '/helpers/es6/promises-with-finally',
+      // },
+      // {
+      //   id: '3.7',
+      //   title: 'Object function assignment',
+      //   path: '/helpers/es6/object-function-assignment',
+      // },
+      // {
+      //   id: '3.8',
+      //   title: 'Object.entries()',
+      //   path: '/helpers/es6/object-entries',
+      // },
+      // {
+      //   id: '3.9',
+      //   title: 'Object Spread operator',
+      //   path: '/helpers/es6/object-spread-operator',
+      // },
+      // {
+      //   id: '4.0',
+      //   title: 'Destructuring Nested Objects',
+      //   path: '/helpers/es6/destructuring-nested-objects',
+      // },
     ],
   },
-  {
-    id: 3,
-    title: 'JavaScript Arrays Sheet',
-    key: 'arrays',
-    subItem: [
-      {
-        id: '3.1',
-        title: 'Assigning array items to variables',
-        path: '/helpers/arrays/assigning-array-items-to-variables',
-      },
-      {
-        id: '3.2',
-        title: 'Skipping items',
-        path: '/helpers/arrays/skipping-items',
-      },
-      {
-        id: '3.3',
-        title: 'Assigning the first values, storing the rest together',
-        path: '/helpers/arrays/assigning-the-first-values-storing-the-rest-together',
-      },
-      {
-        id: '3.4',
-        title: 'Swapping variables',
-        path: '/helpers/arrays/swapping-variables',
-      },
-    ],
-  },
-  {
-    id: 4,
-    title: 'JavaScript Strings Sheet',
-    key: 'strings',
-    subItem: [
-      {
-        id: '4.1',
-        title: 'charAt()',
-        path: '/helpers/strings/char-at',
-      },
-      {
-        id: '4.2',
-        title: 'concat()',
-        path: '/helpers/strings/concat',
-      },
-      {
-        id: '4.3',
-        title: 'indexOf()',
-        path: '/helpers/strings/index-of',
-      },
-      {
-        id: '4.4',
-        title: 'lastIndexOf()',
-        path: '/helpers/strings/last-index-of',
-      },
-      {
-        id: '4.5',
-        title: 'replace()',
-        path: '/helpers/strings/replace',
-      },
-      {
-        id: '4.6',
-        title: 'search()',
-        path: '/helpers/strings/search',
-      },
-      {
-        id: '4.7',
-        title: 'slice(start, end)',
-        path: '/helpers/strings/slice',
-      },
-      {
-        id: '4.8',
-        title: 'substr(start, length)',
-        path: '/helpers/strings/substr',
-      },
-      {
-        id: '4.9',
-        title: 'substring(start, end)',
-        path: '/helpers/strings/substring',
-      },
-      {
-        id: '5.0',
-        title: 'toUpperCase()',
-        path: '/helpers/strings/to-upper-case',
-      },
-      {
-        id: '5.1',
-        title: 'toLowerCase()',
-        path: '/helpers/strings/to-lower-case',
-      },
-      {
-        id: '5.2',
-        title: 'valueOf()',
-        path: '/helpers/strings/value-of',
-      },
-      {
-        id: '5.3',
-        title: 'trim()',
-        path: '/helpers/strings/trim',
-      },
-      {
-        id: '5.4',
-        title: 'toString()',
-        path: '/helpers/strings/to-string',
-      },
-      {
-        id: '5.5',
-        title: 'includes()',
-        path: '/helpers/strings/includes',
-      },
-      {
-        id: '5.6',
-        title: 'charCodeAt()',
-        path: '/helpers/strings/char-code-at',
-      },
-      {
-        id: '5.7',
-        title: 'match()',
-        path: '/helpers/strings/match',
-      },
-      {
-        id: '5.8',
-        title: 'split()',
-        path: '/helpers/strings/split',
-      },
-    ],
-  },
-  {
-    id: 5,
-    title: 'JavaScript Async/Await',
-    key: 'async-await',
-    subItem: [
-      {
-        id: '5.1',
-        title: 'Async',
-        path: '/helpers/async-await/async',
-      },
-      {
-        id: '5.2',
-        title: 'Await',
-        path: '/helpers/async-await/await',
-      },
-    ],
-  },
-  {
-    id: 6,
-    title: 'JavaScript API Calls',
-    key: 'api-calls',
-    subItem: [
-      {
-        id: '6.1',
-        title: 'XML HTTP Request',
-        path: '/helpers/api-calls/xml-http-request',
-      },
-      {
-        id: '6.2',
-        title: 'Fetch',
-        path: '/helpers/api-calls/fetch',
-      },
-      {
-        id: '6.3',
-        title: 'Axios',
-        path: '/helpers/api-calls/axios',
-      },
-      {
-        id: '6.4',
-        title: 'JQuery AJAX',
-        path: '/helpers/api-calls/jquery-ajax',
-      },
-      {
-        id: '6.5',
-        title: 'useSwr',
-        path: '/helpers/api-calls/use-swr',
-      },
-    ],
-  },
+  // {
+  //   id: 3,
+  //   title: 'JavaScript Arrays Sheet',
+  //   key: 'arrays',
+  //   subItem: [
+  //     {
+  //       id: '3.1',
+  //       title: 'Assigning array items to variables',
+  //       path: '/helpers/arrays/assigning-array-items-to-variables',
+  //     },
+  //     {
+  //       id: '3.2',
+  //       title: 'Skipping items',
+  //       path: '/helpers/arrays/skipping-items',
+  //     },
+  //     {
+  //       id: '3.3',
+  //       title: 'Assigning the first values, storing the rest together',
+  //       path: '/helpers/arrays/assigning-the-first-values-storing-the-rest-together',
+  //     },
+  //     {
+  //       id: '3.4',
+  //       title: 'Swapping variables',
+  //       path: '/helpers/arrays/swapping-variables',
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 4,
+  //   title: 'JavaScript Strings Sheet',
+  //   key: 'strings',
+  //   subItem: [
+  //     {
+  //       id: '4.1',
+  //       title: 'charAt()',
+  //       path: '/helpers/strings/char-at',
+  //     },
+  //     {
+  //       id: '4.2',
+  //       title: 'concat()',
+  //       path: '/helpers/strings/concat',
+  //     },
+  //     {
+  //       id: '4.3',
+  //       title: 'indexOf()',
+  //       path: '/helpers/strings/index-of',
+  //     },
+  //     {
+  //       id: '4.4',
+  //       title: 'lastIndexOf()',
+  //       path: '/helpers/strings/last-index-of',
+  //     },
+  //     {
+  //       id: '4.5',
+  //       title: 'replace()',
+  //       path: '/helpers/strings/replace',
+  //     },
+  //     {
+  //       id: '4.6',
+  //       title: 'search()',
+  //       path: '/helpers/strings/search',
+  //     },
+  //     {
+  //       id: '4.7',
+  //       title: 'slice(start, end)',
+  //       path: '/helpers/strings/slice',
+  //     },
+  //     {
+  //       id: '4.8',
+  //       title: 'substr(start, length)',
+  //       path: '/helpers/strings/substr',
+  //     },
+  //     {
+  //       id: '4.9',
+  //       title: 'substring(start, end)',
+  //       path: '/helpers/strings/substring',
+  //     },
+  //     {
+  //       id: '5.0',
+  //       title: 'toUpperCase()',
+  //       path: '/helpers/strings/to-upper-case',
+  //     },
+  //     {
+  //       id: '5.1',
+  //       title: 'toLowerCase()',
+  //       path: '/helpers/strings/to-lower-case',
+  //     },
+  //     {
+  //       id: '5.2',
+  //       title: 'valueOf()',
+  //       path: '/helpers/strings/value-of',
+  //     },
+  //     {
+  //       id: '5.3',
+  //       title: 'trim()',
+  //       path: '/helpers/strings/trim',
+  //     },
+  //     {
+  //       id: '5.4',
+  //       title: 'toString()',
+  //       path: '/helpers/strings/to-string',
+  //     },
+  //     {
+  //       id: '5.5',
+  //       title: 'includes()',
+  //       path: '/helpers/strings/includes',
+  //     },
+  //     {
+  //       id: '5.6',
+  //       title: 'charCodeAt()',
+  //       path: '/helpers/strings/char-code-at',
+  //     },
+  //     {
+  //       id: '5.7',
+  //       title: 'match()',
+  //       path: '/helpers/strings/match',
+  //     },
+  //     {
+  //       id: '5.8',
+  //       title: 'split()',
+  //       path: '/helpers/strings/split',
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 5,
+  //   title: 'JavaScript Async/Await',
+  //   key: 'async-await',
+  //   subItem: [
+  //     {
+  //       id: '5.1',
+  //       title: 'Async',
+  //       path: '/helpers/async-await/async',
+  //     },
+  //     {
+  //       id: '5.2',
+  //       title: 'Await',
+  //       path: '/helpers/async-await/await',
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 6,
+  //   title: 'JavaScript API Calls',
+  //   key: 'api-calls',
+  //   subItem: [
+  //     {
+  //       id: '6.1',
+  //       title: 'XML HTTP Request',
+  //       path: '/helpers/api-calls/xml-http-request',
+  //     },
+  //     {
+  //       id: '6.2',
+  //       title: 'Fetch',
+  //       path: '/helpers/api-calls/fetch',
+  //     },
+  //     {
+  //       id: '6.3',
+  //       title: 'Axios',
+  //       path: '/helpers/api-calls/axios',
+  //     },
+  //     {
+  //       id: '6.4',
+  //       title: 'JQuery AJAX',
+  //       path: '/helpers/api-calls/jquery-ajax',
+  //     },
+  //     {
+  //       id: '6.5',
+  //       title: 'useSwr',
+  //       path: '/helpers/api-calls/use-swr',
+  //     },
+  //   ],
+  // },
 ];
