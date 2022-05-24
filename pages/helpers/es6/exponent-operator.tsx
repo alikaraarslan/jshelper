d622e443b1473db053b0bc5ccb4e32716836cad8
Ali Karaarslan <ali.karaarslan@bilgeadam.com>
2022-05-25 01:17:02 +0300

    add es6/exponent-operator

--- pages/helpers/es6/exponent-operator.tsx
+++ pages/helpers/es6/exponent-operator.tsx
@@ -1,5 +1,48 @@
+/* eslint-disable react/no-unescaped-entities */
+import { CopyBlock, vs2015 } from 'react-code-blocks';
+
 const ExponentOperator = () => {
-  return <div>ExponentOperator</div>;
+  return (
+    <div className="content-item">
+      <h2>Exponent Operator</h2>
+      <div className="desc">
+        <p>
+          The exponentiation operator (**) returns the result of raising the
+          first operand to the power of the second operand. It is equivalent to
+          Math.pow, except it also accepts BigInts as operands.
+        </p>
+        <div className="code-block">
+          <CopyBlock
+            text={`const byte = 2 ** 8
+// Same as: Math.pow(2, 8)
+`}
+            language={'javascript'}
+            showLineNumbers={false}
+            theme={vs2015}
+          />
+        </div>
+        <div className="code-block">
+          <CopyBlock
+            text={`console.log(3 ** 4);
+// expected output: 81
+
+console.log(10 ** -2);
+// expected output: 0.01
+
+console.log(2 ** 3 ** 2);
+// expected output: 512
+
+console.log((2 ** 3) ** 2);
+// expected output: 64
+`}
+            language={'javascript'}
+            showLineNumbers={false}
+            theme={vs2015}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ExponentOperator;
